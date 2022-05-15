73afcc6b7b676a39cd14cc062b02a676aa8e6635
minhphucdev201 <minhphuc201.dev@gmail.com>
2022-05-15 10:45:22 +0700

    lib

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/components/HomePage';
+import "antd/dist/antd.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
