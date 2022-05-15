f31867c50da0390816e783b5173ddcf0f677f6bf
minhphucdev201 <minhphuc201.dev@gmail.com>
2022-05-15 12:42:23 +0700

    assets

--- src/pages/components/HomePage/index.jsx
+++ src/pages/components/HomePage/index.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Button, DatePicker, version } from "antd";
+import React from "react";
+import PropTypes from "prop-types";
+import { Layout } from "antd";
+import { Row, Col } from "antd";
 
-HomePage.propTypes = {
-    
-};
+const { Header, Footer, Sider, Content } = Layout;
+HomePage.propTypes = {};
 
 function HomePage(props) {
-    return (
-        <div>
-      <Button type="primary" style={{ marginLeft: 8 }}>
-      Primary Button
-    </Button>
-  </div>
-    );
+  return (
+    <div>
+      <Layout>
+        <Header style={{ background: "red" }}>
+          <div>
+            <Row>
+              <Col span={10}>Menu</Col>
+              <Col span={4}>Branch</Col>
+              <Col span={10}>Feature</Col>
+            </Row>
+          </div>
+        </Header>
+      </Layout>
+    </div>
+  );
 }
 
 export default HomePage;
